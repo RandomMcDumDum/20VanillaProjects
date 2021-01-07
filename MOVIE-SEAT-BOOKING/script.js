c1186bd26468e1c1343d110780abb2f11dedee37
Chris <christianguerrero98@gmail.com>
2021-01-07 12:23:47 -0600

    Added local storage functionality to the program. Browser will now keep track of Movie that is selected as well as any seats that were selected before browser refresh.

--- MOVIE-SEAT-BOOKING/script.js
+++ MOVIE-SEAT-BOOKING/script.js
@@ -7,9 +7,25 @@ const movieSelect = document.getElementById('movie')
 let ticketPrice = +movieSelect.value;
 // console.log(typeof ticketPrice);
 
+// SAVE SELECTED MOVIE INDEX AND PRICE
+function setMovieData (movieIndex, moviePrice){
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // UPDATE TOTAL AND COUNT
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    // Copy the selected seats into an array
+    // Map through array
+    // Return an array of indexes
+    const seatsIndex = [...selectedSeats].map((seat)=>[...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    console.log(seatsIndex);
+
 
     const selectedSeatsCount = selectedSeats.length;
 
@@ -20,6 +36,9 @@ function updateSelectedCount(){
 // MOVIE SELECT EVENT
 movieSelect.addEventListener('change', function (e){
     ticketPrice = +e.target.value
+
+    setMovieData(e.target.selectedIndex, e.target.value);
+
     updateSelectedCount();
 })
 
