061883d3ee294ab191744cdcdf19d67a4c4c3cce
Chris <christianguerrero98@gmail.com>
2021-01-08 09:01:00 -0600

    Added Progress bar functionality. Current video time now changes when the progress bar is moved. Timestamp also updates now with the current time the player is at in the video. Project, done.

--- CUSTOM-VIDEO-PLAYER/script.js
+++ CUSTOM-VIDEO-PLAYER/script.js
@@ -23,13 +23,28 @@ function updatePlayIcon(){
 }
 
 // UPDATE PROGRESS AND TIMESTAMP
-function updateProgress (){
-    return true;
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10) {
+        mins = '0' + String(mins);
+    }
+
+    // get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timeStamp.innerHTML = `${mins}:${secs}`
+
 }
 
 // SET VIDEO TIME TO PROGRESS
 function setVideoProgress(){
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // STOP VIDEO
@@ -43,7 +58,7 @@ function stopVideo (){
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timeUpdate', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
