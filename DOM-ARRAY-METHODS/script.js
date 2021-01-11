1f6b8317e34630ddd6a380b0e0371287475da18b
Chris <christianguerrero98@gmail.com>
2021-01-11 10:44:17 -0600

    Updated dom to react to randomUser Generation. Used forEach method to work with each person in the array at a time. Also added an event listener to add user button to call generateRandomUser function and append our arr

--- DOM-ARRAY-METHODS/script.js
+++ DOM-ARRAY-METHODS/script.js
@@ -22,10 +22,33 @@ async function getRandomUser(){
         name: `${user.name.first} ${user.name.last}`,
         money: Math.floor(Math.random() * 1000000)
     }
-    console.log(newUser)
+    addData(newUser);
 }
 
 // Add new obj to data arr
-function addData(){
+function addData(obj){
     data.push(obj);
+
+    updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data){
+    // clear the main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach( item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<Strong>${item.name}</Strong>${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+}
+
+// Format number as money
+function formatMoney(number){
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// EVENT LISTENERS
+addUserBtn.addEventListener('click', getRandomUser);
