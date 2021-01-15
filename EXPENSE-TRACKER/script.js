3c857d83204fd32c7ffd6d0fde3ee0f494fc5bd3
Chris <christianguerrero98@gmail.com>
2021-01-15 12:45:31 -0600

    Added a way to add transactions through a function that takes values of the form inputs. On submit, it makes sure there are values there and then updates the DOM accordingly using previously created functions. Also added an inline function to the delete button that sends the ID of the transaction being clicked and creates a new arary, filtering out any objects that have the ID of the transcation being clicked.

--- EXPENSE-TRACKER/script.js
+++ EXPENSE-TRACKER/script.js
@@ -15,6 +15,34 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+// Add Transaction
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+        alert('please add a text and amount')
+    }else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        } 
+
+        transactions.push(transaction);
+        addTransactionDOM(transaction);
+        updateValues(transaction);
+
+        text.value = '';
+        amount.value = '';
+
+    }
+
+   
+}
+
+// Generate Random ID
+function generateID(){    return Math.floor(Math.random() * 100000000);}
+
 // add transactions to DOM list
 function addTransactionDOM(transaction) {
     // Get sign
@@ -25,7 +53,7 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
     item.innerHTML = `
-        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class='delete-btn'>x</button>
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class='delete-btn' onclick='removeTransaction(${transaction.id})'>x</button>
     `;
 
     list.appendChild(item);
@@ -51,6 +79,13 @@ function updateValues() {
     money_minus.innerText = `$${expense}`;
 }
 
+// Remove transaction by ID
+function removeTransaction(id) {
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    init();
+}
+
 // init APP
 function init(){
     list.innerHTML = '';
@@ -60,3 +95,5 @@ function init(){
 }
 
 init();
+
+form.addEventListener('submit', addTransaction)
