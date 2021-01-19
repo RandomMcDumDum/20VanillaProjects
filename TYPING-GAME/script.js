23217a184505f09f346045e4e07cc8521775fdf6
Chris <christianguerrero98@gmail.com>
2021-01-19 10:21:07 -0600

    set the settings button to toggle the difficulty bar. Also implemented difficulty using local storage to see what difficulty is currently selected as well as making sure that the player knows what difficulty they are at. Also added an if statement to the text field listener to add time for every word based on the difficulty.

--- TYPING-GAME/script.js
+++ TYPING-GAME/script.js
@@ -41,6 +41,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Set difficulty to value in local storage or medium
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+// Set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
 // Focus on text on start
 text.focus();
 
@@ -90,6 +96,9 @@ function gameOver() {
 
 addWordToDOM();
 
+// Event Listeners
+
+// Player typing
 text.addEventListener('input', e => {
     const insertedText = e.target.value;
 
@@ -100,8 +109,23 @@ text.addEventListener('input', e => {
         // Clear
         e.target.value = '';
 
-        time += 5;
+        if(difficulty === 'hard'){
+            time += 2;
+        }else if(difficulty === 'medium'){
+            time += 3;
+        }else {
+            time += 5;
+        }
 
         updateTime();
     }
 });
+
+// Settings btn click
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// settings select
+settingsForm.addEventListener('change', e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+})
