b5d621b3176b8291a5377becf210af4f3172fa6c
Chris <christianguerrero98@gmail.com>
2021-01-20 11:29:58 -0600

    Just followed the step of the tut. (I wasnt trying to much today) Neat API though

--- TEXT-SPEECH-READER/script.js
+++ TEXT-SPEECH-READER/script.js
@@ -1,9 +1,9 @@
-const main= document.querySelector('main');
+const main = document.querySelector('main');
 const voicesSelect = document.getElementById('voices');
 const textArea = document.getElementById('text');
 const readBtn = document.getElementById('read');
 const toggleBtn = document.getElementById('toggle');
-const close = document.getElementById('close');
+const closeBtn = document.getElementById('close');
 
 const data = [
     {
@@ -70,7 +70,68 @@ function createBox(item) {
         <p class="info">${text}</p>
     `;
 
-    // @todo - speak event
+    box.addEventListener('click', () => {
+        setTextMessage(text);
+        speakText();
+
+        // Add active effect
+        box.classList.add('active');
+        setTimeout(() => box.classList.remove('active'), 800);
+    });
 
     main.appendChild(box);
 }
+
+// Init Speech synth
+const message = new SpeechSynthesisUtterance();
+
+// Store voice
+let voices = [];
+
+function getVoices() {
+    voices = speechSynthesis.getVoices();
+
+    voices.forEach(voice => {
+        const option = document.createElement('option');
+
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`;
+
+        voicesSelect.appendChild(option);
+    });
+}
+
+// Set text
+function setTextMessage(text) {
+    message.text = text;
+}
+
+// speak Text
+function speakText() {
+    speechSynthesis.speak(message);
+}
+
+// Set voice
+function setVoice(e){
+    message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+// Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener('click', () => document.getElementById('text-box').classList.toggle('show'));
+
+// Close text box
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
+
+// Change voice
+voicesSelect.addEventListener('change', setVoice);
+
+// Read text button
+readBtn.addEventListener('click', () => {
+    setTextMessage(textArea.value);
+    speakText();
+})
+
+getVoices();
