876580b9c4c0f2ba1febc1d3e6cea3a4c63795eb
Chris <christianguerrero98@gmail.com>
2021-01-22 12:52:43 -0600

    Added listener to getLyrics buttons and created a function that calls the API again to grab the lyrics that belong to that artist and songtitle of the attributes set in the button being clicked. After we get results, we set the DOM to show the artist - song title, and then use RegEx to format the lyrics given to us.

--- LYRICS-SEARCH/script.js
+++ LYRICS-SEARCH/script.js
@@ -62,6 +62,21 @@ async function getMoreSongs(url){
     showData(data);
 }
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+    result.innerHTML = `
+        <h2><strong>${artist}</strong> - ${songTitle}</h2>
+        <span>${lyrics}</span>
+    `;
+
+    more.innerHTML = '';
+}
+
 // Event Listeners
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -72,5 +87,18 @@ form.addEventListener('submit', e => {
         alert('Please Type in a Search Term');
     }else {
         searchSongs(searchTerm);
+    }
+});
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+    
+    if (clickedEl.tagName === "BUTTON") {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist, songTitle);
+
     }
 })
