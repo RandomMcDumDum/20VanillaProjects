9c70a3e6ab91c4d26d3e346c7b305aeef0e1b7a6
Chris <christianguerrero98@gmail.com>
2021-01-26 13:46:08 -0600

    update() calls itself with the requestAnimationFrame so every frame it refreshes the canvas. added a line in the draw() to clear the canvas first and draw everything again. Add listeners to the DOM to listen for keyDown and keyUp and created functions that move and stop the paddle when the left or right arrow keys are pressed

--- BREAKOUT-GAME/script.js
+++ BREAKOUT-GAME/script.js
@@ -26,7 +26,7 @@ const paddle = {
     w: 80,
     h: 10,
     speed: 8,
-    dc: 0
+    dx: 0
 }
 
 // Create brick props
@@ -87,14 +87,62 @@ function drawBricks() {
     })
 }
 
+// Move paddle on canvas
+function movePaddle() {
+    paddle.x += paddle.dx
+
+    // Wall detection
+    if(paddle.x + paddle.w > canvas.width){
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if(paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
+
 // Draw Everyting
 function draw(){
+    // Clear canvas
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
     drawBall();
     drawPaddle();
     drawScore();
     drawBricks();
 }
-draw();
+
+// Update canvas drawing and animaton
+function update() {
+    movePaddle();
+
+    // Draw everything
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight'){
+        paddle.dx = paddle.speed;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = -paddle.speed;
+    }
+}
+
+// keyup event
+function keyUp(e){
+    if(e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft'){
+        paddle.dx = 0;
+    }
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 // Rules and close event Handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
