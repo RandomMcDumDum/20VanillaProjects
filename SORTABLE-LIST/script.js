3ffcf98deae450ad76bb0fde02798657d16764a1
RandomMcDumDum <71903478+RandomMcDumDum@users.noreply.github.com>
2021-02-01 15:39:42 -0600

    Added click event to checkBtn that calls a function that runs the listItems accross the main richestPeople[] and gives classes to the elemnts, right or wrong,  based on on the value and index of listItem in comparison to the arr.

--- SORTABLE-LIST/script.js
+++ SORTABLE-LIST/script.js
@@ -72,12 +72,27 @@ function dragDrop(){
     this.classList.remove('over');
 }
 
+// Swap list items that are drag and drop
 function swapItems(fromIndex, toIndex) {
     const itemOne = listItems[fromIndex].querySelector('.draggable');
     const itemTwo = listItems[toIndex].querySelector('.draggable');
 
     listItems[fromIndex].appendChild(itemTwo);
     listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of listItems
+function checkOrder(){
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector('.draggable').innerText.trim();
+
+        if(personName !== richestPeople[index]){
+            listItem.classList.add('wrong');
+        }else {
+            listItem.classList.remove('wrong');
+            listItem.classList.add('right');
+        }
+    });
 }
 
 function addEventListeners() {
@@ -95,3 +110,5 @@ function addEventListeners() {
         item.addEventListener('dragleave', dragLeave);
     });
 }
+
+checkBtn.addEventListener('click', checkOrder);
